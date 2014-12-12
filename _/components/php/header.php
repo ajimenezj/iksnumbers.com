9607681f2293fa6b98d7efdfabdfc4d95b2a8bd2
Arnaldo Jimenez <webform@galaxydesign.org>
2014-12-12 17:15:06 -0400

    Agregue el dropdown menu
    A la pantalla principal ya esta funcionando el dropdown-menu

--- _/components/php/header.php
+++ _/components/php/header.php
@@ -2,8 +2,27 @@
 	<div class="col-lg-12">
 		<header class="clearfix">
 			<section class="branding">
-				<a href="index.php"></a>
+				<a href="index.php"><img src="images/logo.png" alt="IKS private server iksnumbers the best iksprivateserver"></img></a>
 			</section> <!-- branding -->
+
+			<section class="navbar">
+				<ul class="nav navbar-nav">
+					<li><a href="index.php" class="active">Home</a></li>
+					<li><a href="westernunion.php">Western union payment</a></li>
+					<li class="dropdown">
+						<a href="#" class="dropdown-toggle" data-toggle="dropdown">Support <span class="caret"></span></a>
+						<ul class="dropdown-menu" role="menu" aria-labelledby="dropdownMenu">
+							<li><a tabindex="-1" href="http://iksnumbers.info/index.php?a=add" target="_blank">Open Support ticket </a></li>
+							<li><a tabindex="-1" href="http://iksnumbers.info/knowledgebase.php" target="_blank">Helpdesk </a></li>
+							<li class="divider"></li>
+							<li><a tabindex="-1" href="http://iksnumbers.info/knowledgebase.php?category=11" target="_blank">Sonicview</a></li>
+							<li><a tabindex="-1" href="http://iksnumbers.info/knowledgebase.php?category=12" target="_blank">Viewsat</a></li>
+							<li><a tabindex="-1" href="http://iksnumbers.info/knowledgebase.php?category=14" target="_blank">I-link</a></li>
+						</ul>
+					</li>
+					<li><a href="contact.php">Contact us</a></li>
+				</ul><!-- nav -->
+			</section><!-- navbar -->
 		</header> <!-- header -->
 	</div> <!-- column -->
 </div> <!-- content -->

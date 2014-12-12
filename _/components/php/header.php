84632bf7de8aba04406ca169c5c3042ae9ceb31b
Arnaldo Jimenez <webform@galaxydesign.org>
2014-12-12 14:26:49 -0400

    agregue el header de la pagina

---
+++ _/components/php/header.php
@@ -0,0 +1,9 @@
+<div class="content row">
+	<div class="col-lg-12">
+		<header class="clearfix">
+			<section class="branding">
+				<a href="index.php"></a>
+			</section> <!-- branding -->
+		</header> <!-- header -->
+	</div> <!-- column -->
+</div> <!-- content -->

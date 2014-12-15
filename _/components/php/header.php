d501ce80c864ad8e2e384d1885a5c38d9d3bf34b
Arnaldo Jimenez <webform@galaxydesign.org>
2014-12-15 17:42:08 -0400

    Agregue el nuevo navbar de la pagina
    Agregue el nuevo navbar de la pagina

--- _/components/php/header.php
+++ _/components/php/header.php
@@ -5,8 +5,50 @@
 				<a href="index.php"><img src="images/logo.png" alt="IKS private server iksnumbers the best iksprivateserver"></img></a>
 			</section> <!-- branding -->
 
-			<section class="navbar">
-				<ul class="nav navbar-nav">
+			<div class="container">
+			  <div class="row">
+			    <nav class="navbar navbar-default navbar-inverse" role="navigation">
+			      <div class="container-fluid">
+			        <!-- Brand and toggle get grouped for better mobile display -->
+			        <div class="navbar-header">
+			          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+			            <span class="sr-only">Toggle navigation</span>
+			            <span class="icon-bar"></span>
+			            <span class="icon-bar"></span>
+			            <span class="icon-bar"></span>
+			          </button>
+			        </div>
+
+			        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+			          <ul class="nav navbar-nav">
+			            <li><a href="index.php">Home</a></li>
+			            <li><a href="westernunion.php">Western union payment</a></li>
+			            <li class="dropdown">
+			              <a href="#" class="dropdown-toggle" data-toggle="dropdown">Support <span class="caret"></span></a>
+			              	<ul class="dropdown-menu" role="menu" aria-labelledby="dropdownMenu">
+								<li><a tabindex="-1" href="http://iksnumbers.info/index.php?a=add" target="_blank">Open Support ticket </a></li>
+								<li><a tabindex="-1" href="http://iksnumbers.info/knowledgebase.php" target="_blank">Helpdesk </a></li>
+								<li class="divider"></li>
+								<li><a tabindex="-1" href="http://iksnumbers.info/knowledgebase.php?category=11" target="_blank">Sonicview</a></li>
+								<li><a tabindex="-1" href="http://iksnumbers.info/knowledgebase.php?category=12" target="_blank">Viewsat</a></li>
+								<li><a tabindex="-1" href="http://iksnumbers.info/knowledgebase.php?category=14" target="_blank">I-link</a></li>
+							</ul>
+			              <li><a href="contact.php">Contact us</a></li>
+			            </li>
+			          </ul>
+			          <form class="navbar-form navbar-right" role="search">
+			            <div class="form-group">
+			              <input type="text" class="form-control" placeholder="Search">
+			            </div>
+			            <button type="submit" class="btn btn-primary">Submit</button>
+			          </form>
+			        </div><!-- /.navbar-collapse -->
+			      </div><!-- /.container-fluid -->
+			    </nav>
+			</div>
+
+				<!--
+				<ul class="navbar navbar-default navbar-inverse">
 					<li><a href="index.php">Home</a></li>
 					<li><a href="westernunion.php">Western union payment</a></li>
 					<li class="dropdown">
@@ -21,8 +63,10 @@
 						</ul>
 					</li>
 					<li><a href="contact.php">Contact us</a></li>
-				</ul><!-- nav -->
-			</section><!-- navbar -->
+				</ul>--><!-- nav -->
+
+
+			
 		</header> <!-- header -->
 	</div> <!-- column -->
 </div> <!-- content -->
